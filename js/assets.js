8d88d0e0db29a4e52eda19875c4d985ad6184169
Fabien Petitjean <f.petitjean@lombardodier.com>
2018-09-27 18:08:41 +0200

    Les 5 jeux sont faits... Ouf, dans les temps.

---
+++ js/assets.js
@@ -0,0 +1,139 @@
+/** @module assets */require( 'assets', function(require, module, exports) { var _=function(){var D={"en":{},"fr":{}},X=require("$").intl;function _(){return X(D,arguments);}_.all=D;return _}();
+    "use strict";
+
+exports.fetch = fetchAssets;
+
+
+function fetchAssets( assets ) {
+  return new Promise(function (resolve, reject) {
+    showSplashScreen().then(function() {
+      var count = 0;
+      var done = 0;
+      for( var key in assets ) count++;
+      var progress = document.getElementById("PROGRESS");
+      var result = {};
+
+      function next( url ) {
+        done++;
+        var percent = done / count;
+        progress.style.transform = "scaleX(" + percent + ")";
+        console.log( url, (100 * percent).toFixed(0) + "%");
+
+        if( done >= count ) {
+          resolve( result );
+          hideSplashScreen();
+          return;
+        }
+      }
+
+      Object.keys( assets ).forEach(function( key ) {
+        var url = assets[key];
+        if( endsWith( url, "jpg", "png", "gif", "svg" ) ) {
+          var img = new Image();
+          img.crossOrigin = "anonymous";
+          result[key] = img;
+          img.onload = next.bind(null, url);
+          img.onerror = function( ex ) {
+            console.error("Unable to load image \"" + key + "\":", url);
+            console.error( ex );
+            next( url );
+          };
+          img.src = url;
+        } else if( endsWith( url, "ogg", "wav", "mp3" ) ) {
+          var audio = document.createElement("audio");
+          result[key] = audio;
+          var slot = function() {
+            console.log( "Slot for ", url );
+            if( audio._loaded ) return;
+            audio._loaded = true;
+            console.log( "Loaded: ", url );
+            next( url );
+          };
+          audio.addEventListener( "canplay", slot );
+          audio.addEventListener( "loadeddata", slot );
+          window.setTimeout( slot, 3000 );
+          audio.addEventListener( "error", function( ex ) {
+            console.error("Unable to load sound \"" + key + "\":", url);
+            console.error( ex );
+            next( url );
+          });
+          audio.src = url;
+          console.log("Loading audio: ", url);
+        } else {
+          fetch( url ).then(function(response) {
+            if( !response.ok ) throw "";
+            if( endsWith( url, "json" ) ) {
+              return response.json();
+            } else {
+              return response.text();
+            }
+          }).then(function(content) {
+            result[key] = content;
+            next( url );
+          }).catch(function(ex) {
+            console.error("Unable to fetch asset \"" + key + "\": ", url);
+            next( url );
+          });
+        }
+      });
+    });
+  });
+}
+
+
+function showSplashScreen( applicationName ) {
+  if( typeof applicationName === 'undefined' ) applicationName = 'TOLOKOBAN';
+
+  return new Promise(function (resolve, reject) {
+    function show() {
+      var splash = document.createElement( "div" );
+      splash.setAttribute( "id", "SPLASH" );
+      splash.innerHTML = "<div>" + applicationName + "</div><div>" + applicationName + "</div>";
+      document.body.appendChild( splash );
+      var progress = document.createElement( "div" );
+      progress.setAttribute( "id", "PROGRESS" );
+      document.body.appendChild( progress );
+      window.setTimeout(function() {
+        splash.setAttribute( "class", "show-splash" );
+      }, 50);
+      resolve();
+    }
+    if( document.readyState === "complete" ) {
+      show();
+    } else {
+      document.addEventListener( "DOMContentLoaded", show );
+    }
+  });
+}
+
+
+function hideSplashScreen() {
+  var splash = document.getElementById( "SPLASH" );
+  splash.setAttribute( "class", "hide-splash" );
+  var progress = document.getElementById( "PROGRESS" );
+  document.body.removeChild( progress );
+  window.setTimeout(function() {
+    document.body.removeChild( splash );
+  }, 500);
+}
+
+
+function endsWith( text ) {
+  text = text.trim().toLowerCase();
+  var arg;
+  for( var i=1; i<arguments.length; i++ ) {
+    arg = arguments[i].toLowerCase();
+    if( text.substr( text.length - arg.length ) == arg ) return true;
+  }
+  return false;
+}
+
+
+  
+module.exports._ = _;
+/**
+ * @module assets
+ * @see module:$
+
+ */
+});
---
+++ js/main.js
@@ -0,0 +1,146 @@
+/** @module main */require( 'main', function(require, module, exports) { var _=function(){var D={"en":{"welcome":"Welcome in the world of"},"fr":{"welcome":"Bienvenue dans le monde de"}},X=require("$").intl;function _(){return X(D,arguments);}_.all=D;return _}();
+    "use strict";
+
+require("font.josefin");
+var $ = require("dom");
+var PM = require("tfw.binding.property-manager");
+var Nim = require("challenge.nim");
+var Main = require("challenge.main");
+var Sound = require("sound");
+var Paper = require("challenge.paper");
+var Taquin = require("challenge.taquin");
+var Assets = require("assets");
+var Twister = require("challenge.twister");
+var Anagram = require("challenge.anagram");
+
+var g_mainView;
+var g_currentView;
+
+exports.start = function() {
+  Assets.fetch({
+    background: "css/main/background.jpg",
+    anagram: "css/challenge.main/anagram.png",
+    nim: "css/challenge.main/nim.png",
+    paper: "css/challenge.main/paper.svg",
+    taquin: "css/challenge.main/taquin.png",
+    twister: "css/challenge.main/twister.svg",
+    victory: "css/challenge.main/victory.png",
+    nim2: "css/challenge.nim/nim.png",
+    paper2: "css/challenge.paper/paper.png",
+    scissors2: "css/challenge.paper/scissors.png",
+    stone2: "css/challenge.paper/stone.png",
+    well2: "css/challenge.paper/well.png",
+    taquin21: "css/challenge.taquin/picture-1.png",
+    taquin22: "css/challenge.taquin/picture-2.png",
+    taquin23: "css/challenge.taquin/picture-3.png",
+    twisterDisk: "css/challemge.twister/disk.svg",
+    twisterNeedle: "css/challemge.twister/needle.svg"
+  }).then(function() {
+    createMainView();
+    g_currentView = g_mainView;
+
+    document.addEventListener( 'keydown', function( evt ) {
+      g_currentView.keydown( evt.key );
+    });
+  });
+};
+
+
+function createMainView() {
+  var view = new Main();
+  $.add( document.body, view );
+  g_mainView = view;
+  PM( view ).on( "challenge", function( name ) {
+    switch( name ) {
+    case "anagram": return createAnagram();
+    case "twister": return createTwister();
+    case "paper": return createPaper();
+    case "taquin": return createTaquin();
+    case "nim": return createNim();
+    }
+  });
+}
+
+function createAnagram() {
+  var view = new Anagram();
+  view.start = 1;
+  showView( view );
+}
+
+
+function createTwister() {
+  var view = new Twister();
+  showView( view );
+}
+
+
+function createNim() {
+  var view = new Nim();
+  showView( view );
+}
+
+
+function createPaper() {
+  var view = new Paper();
+  showView( view );
+}
+
+
+function createTaquin() {
+  var view = new Taquin();
+  showView( view );
+}
+
+
+function showView( view ) {
+  $.addClass( view, "hide" );
+  $.add( document.body, view );
+  window.setTimeout(function() {
+    $.removeClass( view, "hide" );
+    window.setTimeout(function() {
+      $.addClass( view, "show" );
+    });
+  });
+  g_currentView = view;
+  var close = function() {
+    $.removeClass( view, "show" );
+    window.setTimeout(function() {
+      $.addClass( view, "hide" );
+    });
+    window.setTimeout(function() {
+      $.detach( view );
+    }, 1000);
+    g_currentView = g_mainView;
+  };
+  PM( view ).on( "success", function() {
+    Sound.play( "success" );
+    g_mainView.forward = true;
+    close();
+  });
+  PM( view ).on( "failure", function() {
+    Sound.play( "failure" );
+    g_mainView.forward = false;
+    close();
+  });
+}
+
+
+  
+module.exports._ = _;
+/**
+ * @module main
+ * @see module:$
+ * @see module:font.josefin
+ * @see module:dom
+ * @see module:tfw.binding.property-manager
+ * @see module:challenge.nim
+ * @see module:challenge.main
+ * @see module:sound
+ * @see module:challenge.paper
+ * @see module:challenge.taquin
+ * @see module:assets
+ * @see module:challenge.twister
+ * @see module:challenge.anagram
+
+ */
+});
